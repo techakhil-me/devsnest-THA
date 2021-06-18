2423c7d1ea1ccbb373c16435deb40083b1a2da8a
TechAkhil <43501564+techakhil-me@users.noreply.github.com>
2021-06-19 01:37:40 +0530

    comment added tha 9

--- THA_9/main.js
+++ THA_9/main.js
@@ -1,37 +1,39 @@
 const seat = document.createElement("div");
 seat.className = "seat";
 
-const container = document.querySelector('.bottom')
-const booked = document.querySelector('.booked')
-const remaining = document.querySelector('.remaining')
-
+const container = document.querySelector(".bottom");
+const booked = document.querySelector(".booked");
+const remaining = document.querySelector(".remaining");
 
 var bcount = 0;
 var rcount = 80;
 
-
-
-
 const number = rcount;
 
-for (let i=0; i<number;i++){
-  container.appendChild(seat.cloneNode(true))
+// add seats element number of time
+for (let i = 0; i < number; i++) {
+  container.appendChild(seat.cloneNode(true));
 }
 
-document.addEventListener('click', (e)=>{
-  console.log(e.target)
-  if (e.target.classList.contains('seat')){
-  console.log(e.target.classList.toggle('occupied'))
-    if (e.target.classList.contains('occupied')){
-bcount++;
-rcount--;
-    }
-    else {
+
+// click event listener
+document.addEventListener("click", (e) => {
+  // check if the clicked element is a seat
+  if (e.target.classList.contains("seat")) {
+    // adding a class named occupied
+    console.log(e.target.classList.toggle("occupied"));
+
+    // check if the seat is occupied and add or subtract respectively
+    if (e.target.classList.contains("occupied")) {
+      bcount++;
+      rcount--;
+    } else {
       bcount--;
       rcount++;
     }
-booked.innerHTML = ` ${bcount}`;
-remaining.innerHTML = ` ${rcount}`;
 
+// update the count in the html
+    booked.innerHTML = ` ${bcount}`;
+    remaining.innerHTML = ` ${rcount}`;
   }
-})
+});
